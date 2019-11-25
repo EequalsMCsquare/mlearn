ad11fba17cf192c19b42ac54613dedf466eaa5f4
ZHONG Xiao <zhongxiao0711@gmail.com>
2019-11-26 02:18:14 +1100

    mul and sub

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -39,12 +39,12 @@ class Tensor:
         assert self.requires_grad, "在无梯度记录要求的Tensor上调用backward()"
 
         if grad is None:
-            if self.shape == (0,):
-                grad = Tensor(1.)
+            if self.shape == ():
+                grad = Tensor(1.0)
             else:
                 raise RuntimeError("梯度必须为非空Tensor指定")
         
-        self.grad.data += grad.data
+        self.grad.data = self.grad.data + grad.data
 
 
         for dependency in self.depends_on:
@@ -64,8 +64,6 @@ class Tensor:
         if not isinstance(t2,Tensor):
             t2 = Tensor(t2)
         return add(self,t2)
-
-
 
 
 
@@ -134,4 +132,56 @@ def add(t1: Tensor, t2: Tensor) -> Tensor:
                     requires_grad,
                     depends_on) 
 
+def mul(t1:Tensor, t2:Tensor) -> Tensor:
+    data = t1.data * t2.data
+    requires_grad = t1.requires_grad or t2.requires_grad
+    depends_on: List[Dependency] = []
         
+    if t1.requires_grad:
+        def grad_fn1(grad: np.ndarray) -> np.ndarray:
+            grad = grad * t2.data
+            # Sum out added dims
+            ndims_added = grad.ndim - t1.data.ndim 
+            for _ in range(ndims_added):
+                grad = grad.sum(axis=0)
+
+            # Sum across broadcasted (but non-added dims)
+            for i, dim in enumerate(t1.shape):
+                if dim == 1:
+                    grad = grad.sum(axis=i, keepdims=True)
+            
+            return grad
+
+        depends_on.append(Dependency(t1,grad_fn1))
+    if t2.requires_grad:
+        def grad_fn2(grad:np.ndarray) -> np.ndarray:
+            grad = grad * t1.data
+            # Sum out added dims
+            ndims_added = grad.ndim - t2.data.ndim 
+            for _ in range(ndims_added):
+                grad = grad.sum(axis=0)
+
+            # Sum across broadcasted (but non-added dims)
+            for i, dim in enumerate(t2.shape):
+                if dim == 1:
+                    grad = grad.sum(axis=i, keepdims=True)
+
+            return grad
+
+        depends_on.append(Dependency(t2, grad_fn2))
+    
+    return Tensor(data,
+                    requires_grad,
+                    depends_on) 
+
+def neg(t: Tensor) -> Tensor:
+    data = -t.data
+    requires_grad = t.requires_grad
+    if requires_grad:
+        depends_on = [Dependency(t, lambda x: -x)]
+    else:
+        depends_on = []
+    return Tensor(data, requires_grad, depends_on)
+
+def sub(t1:Tensor, t2:Tensor) -> Tensor:
+    return add(t1,neg(t2))
