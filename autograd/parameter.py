fc65a4848eae615fcbf6eceb6953c842d5f86d5e
ZHONG Xiao <zhongxiao0711@gmail.com>
2019-12-01 22:24:43 +1100

    交叉熵会出现处以0的问题

--- autograd/parameter.py
+++ autograd/parameter.py
@@ -6,7 +6,7 @@ from .tensor import Tensor
 
 class Parameter(Tensor):
     def __init__(self, *shape) -> None:
-        data = np.random.randn(*shape)
+        data = np.random.rand(*shape)
         super().__init__(data, requires_grad=True)
 
     
