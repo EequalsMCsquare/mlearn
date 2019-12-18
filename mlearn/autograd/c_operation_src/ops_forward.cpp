44ca04216aa8c744eb41d1d500f63683fec7ecc7
ZHONG Xiao <zhongxiao0711@gmail.com>
2019-12-19 00:41:13 +1100

    read write operation in C

--- mlearn/autograd/c_operation_src/ops_forward.cpp
+++ mlearn/autograd/c_operation_src/ops_forward.cpp
@@ -1,55 +1,43 @@
 #include "ops_forward.hh"
 
 // Add
-double *__add(double *t1, int t1_shape[], int a_dim, double *t2, int t2shape[],
-              int b_dim) {
-
+void __add(double *out, double *t1, int t1_shape[], int a_dim, double *t2,
+           int t2_shape[], int b_dim) {
   int arr_length = 1;
   for (int i = 0; i < a_dim; i++)
     arr_length *= t1_shape[i];
   double *result = (double *)malloc(arr_length * sizeof(double));
   for (int i = 0; i < arr_length; i++)
-    result[i] = t1[i] + t2[i];
-  return result;
+    out[i] = t1[i] + t2[i];
 }
 
 double *add_forward_1d(int a1, double *array1, int b1, double *array2) {
   int t1_shape[] = {a1};
   int t2_shape[] = {b1};
-
-  return __add(array1, t1_shape, 1, array2, t2_shape, 1);
+  double *result = (double *)malloc(sizeof(double) * a1);
+  __add(result, array1, t1_shape, 1, array2, t2_shape, 1);
+  return result;
 }
+
+
 
 double *add_forward_2d(int a1, int a2, double *array1, int b1, int b2,
                        double *array2) {
   int t1_shape[] = {a1, a2};
   int t2_shape[] = {b1, b2};
-
-  return __add(array1, t1_shape, 2, array2, t2_shape, 2);
+  double *result = (double *)malloc(sizeof(double) * a1 * a2);
+  __add(result, array1, t1_shape, 2, array2, t2_shape, 2);
+  return result;
 }
-// double *add_forward_2d(int a1, int a2, double *array1, int b1, int b2,
-//                        double *array2) {
-//   int t1_shape[] = {a1, a2};
-//   int t2_shape[] = {b1, b2};
-
-//   double *reuslt = (double *)malloc(a1 * a2 * sizeof(double));
-//   std::thread threads[a1];
-//   for (int i = 0; i < a1; i++) {
-//     threads[i] = std::thread(__add,&array1[i * a2], &t1_shape[1], 1,
-//                              &array2[i * b2], &t2_shape[1], 1);
-//   }
-//   for (int i = 0; i < a1; i++) {
-//     threads[i].join();
-//   }
-//   return reuslt;
-// }
 
 double *add_forward_3d(int a1, int a2, int a3, double *array1, int b1, int b2,
                        int b3, double *array2) {
   int t1_shape[] = {a1, a2, a3};
   int t2_shape[] = {b1, b2, b3};
+  double *reuslt = (double *)malloc(a1 * a2 * a3 * sizeof(double));
 
-  return __add(array1, t1_shape, 3, array2, t2_shape, 3);
+  __add(reuslt, array1, t1_shape, 3, array2, t2_shape, 3);
+  return reuslt;
 }
 
 double *add_forward_4d(int a1, int a2, int a3, int a4, double *array1, int b1,
@@ -57,7 +45,10 @@ double *add_forward_4d(int a1, int a2, int a3, int a4, double *array1, int b1,
   int t1_shape[] = {a1, a2, a3, a4};
   int t2_shape[] = {b1, b2, b3, b4};
 
-  return __add(array1, t1_shape, 4, array2, t2_shape, 4);
+  double *reuslt = (double *)malloc(a1 * a2 * a3 * a4 * sizeof(double));
+
+  __add(reuslt, array1, t1_shape, 4, array2, t2_shape, 4);
+  return reuslt;
 }
 
 // neg
@@ -94,7 +85,7 @@ double *sub_forward_2d(int a1, int a2, double *array1, int b1, int b2,
   int t1_shape[] = {a1, a2};
   int t2_shape[] = {b1, b2};
 
-  return __add(array1, t1_shape, 2, array2, t2_shape, 2);
+  return __sub(array1, t1_shape, 2, array2, t2_shape, 2);
 }
 
 double *sub_forward_3d(int a1, int a2, int a3, double *array1, int b1, int b2,
@@ -102,7 +93,7 @@ double *sub_forward_3d(int a1, int a2, int a3, double *array1, int b1, int b2,
   int t1_shape[] = {a1, a2, a3};
   int t2_shape[] = {b1, b2, b3};
 
-  return __add(array1, t1_shape, 3, array2, t2_shape, 3);
+  return __sub(array1, t1_shape, 3, array2, t2_shape, 3);
 }
 
 double *sub_forward_4d(int a1, int a2, int a3, int a4, double *array1, int b1,
@@ -110,5 +101,5 @@ double *sub_forward_4d(int a1, int a2, int a3, int a4, double *array1, int b1,
   int t1_shape[] = {a1, a2, a3, a4};
   int t2_shape[] = {b1, b2, b3, b4};
 
-  return __add(array1, t1_shape, 4, array2, t2_shape, 4);
+  return __sub(array1, t1_shape, 4, array2, t2_shape, 4);
 }
