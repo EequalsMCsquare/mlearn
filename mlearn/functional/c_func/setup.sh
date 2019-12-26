9e129df13448e9a3b66bd1b2cd342c925ff0a419
Xiao ZHONG <zhongxiao0711@gmail.com>
2019-12-26 16:19:55 +1100

    debug

--- mlearn/functional/c_func/setup.sh
+++ mlearn/functional/c_func/setup.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
 
-# No OpenMP
-swig -python c_func.i&&\
-gcc -fPIC -fopenmp -m64 -Ofast -march=native -c c_func.c&&\
-gcc -fPIC -fopenmp -m64 -Ofast -march=native -c c_func_wrap.c -I/home/reco/.local/include/python -I/home/reco/.local/include/numpy/&&\
-gcc -shared -fopenmp -m64 -Ofast -march=native c_func.o  c_func_wrap.o -o _c_func.so&&\
-cp c_func_copy.py c_func.py&&\
-python3 test.py
-
-
-# ## With OpenMP
+# # No OpenMP
 # swig -python c_func.i&&\
-# gcc -fPIC -m64 -Ofast -march=native -fopenmp -c c_func.c&&\
-# gcc -fPIC -m64 -Ofast -march=native -fopenmp -c c_func_wrap.c -I/home/reco/.local/include/python -I/home/reco/.local/include/numpy/&&\
-# gcc -shared -m64 -Ofast -march=native -fopenmp c_func.o  c_func_wrap.o -o _c_func.so&&\
+# gcc -fPIC -fopenmp -m64 -Ofast -march=native -c c_func.c&&\
+# gcc -fPIC -fopenmp -m64 -Ofast -march=native -c c_func_wrap.c -I/home/reco/.local/include/python -I/home/reco/.local/include/numpy/&&\
+# gcc -shared -fopenmp -m64 -Ofast -march=native c_func.o  c_func_wrap.o -o _c_func.so&&\
 # cp c_func_copy.py c_func.py&&\
 # python3 test.py
+
+
+## With OpenMP
+swig -python c_func.i&&\
+gcc -fPIC -m64 -Ofast -march=native -fopenmp -c c_func.c&&\
+gcc -fPIC -m64 -Ofast -march=native -fopenmp -c c_func_wrap.c -I/home/reco/.local/include/python -I/home/reco/.local/include/numpy/&&\
+gcc -shared -m64 -Ofast -march=native -fopenmp c_func.o  c_func_wrap.o -o _c_func.so&&\
+cp c_func_copy.py c_func.py&&\
+python3 test.py
 
 
 # # Only OpenMP and -Ofast
