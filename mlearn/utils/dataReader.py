46eb6a3e09953a10da25ec2fa1cb316c5e61de02
Xiao ZHONG <zhongxiao0711@gmail.com>
2019-12-30 02:44:32 +1100

    数据集读取器

---
+++ mlearn/utils/dataReader.py
@@ -0,0 +1,17 @@
+
+class DataReader:
+    def __init__(self, PATH:str)
+    """
+        PATH should contrain both [FEATURES] and [LABELS]
+    """
+    self.PATH = PATH
+
+
+class IMG_Reader(DataReader):
+    def __init__(self, PATH:str)
+    super(IMG_Reader,self).__init__(PATH)
+
+
+class TEXT_Reader(DataReader):
+    def __init__(self, PATH:str)
+    super(TEXT_Reader,self).__init__(PATH)
