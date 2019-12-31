b567a9516c2cb896f53688626a20c713ca515640
Xiao ZHONG <zhongxiao0711@gmail.com>
2020-01-01 05:18:30 +1100

    增加了Adam优化器

--- mlearn/optimizers/optimizer.py
+++ mlearn/optimizers/optimizer.py
@@ -29,7 +29,7 @@ class SGD(Optimizer):
         Stochastic Gradient Descent
     """
     def __init__(self, module:Module, lr: float = 1e-3) -> None:
-        super(BGD,self).__init__(lr, module)
+        super(SGD,self).__init__(lr, module)
     def step(self) -> None:
         self.iteration += 1
         for parameter in self.module.parameters():
@@ -38,6 +38,10 @@ class SGD(Optimizer):
 
 
 class Momentum(Optimizer):
+    """
+        动量梯度下降
+        Momentum
+    """
     def __init__(self, module: Module, lr: float = 1e-3, momentum: float = 0.9):
         self.module = module
         self.lr = lr
@@ -47,46 +51,55 @@ class Momentum(Optimizer):
             self.v.append(zeros(*parameter.data.shape))
 
     def step(self):
-        v_iter = iter(self.v)
         for i, parameter in enumerate(self.module.parameters()):
-            v = self.momentum * next(v_iter) + (1 - self.momentum) * parameter.grad
+            v = self.momentum * self.v[i] + (1 - self.momentum) * parameter.grad
             self.v[i] = v
             parameter -= self.lr * v
 
 class RMSProp(Optimizer):
-    def __init__(self, module: Module, lr: float = 1e-3, alpha: float = 0.9, eps: float = 1e-7) -> None:
+    def __init__(self, module: Module, lr: float = 1e-3, beta: float = 0.999, eps: float = 1e-7) -> None:
         """
-        Module -> 神经网络模型
-        lr -> 学习率
-        alpha -> 衰减速率
+            Module -> 神经网络模型
+            lr -> 学习率
+            beta -> 权重衰减速率
 
-         MATH:
-                v = alpha * 累计平方梯度 + (1 - alpha) * 参数梯度 ** 2
-                self.v[i] = (lr/sqrt(eps + v)) * 参数梯度
-                参数更新 -= self.v[i]
+             MATH:
+                    s = beta * 累计平方梯度 + (1 - beta) * 参数梯度 ** 2
+                    eta = self.lr * 参数梯度 / sqrt(s + eps)
+                    参数更新 -= eta
         """
         self.module = module
         self.lr = lr
-        self.alpha = alpha
+        self.beta = beta
         self.eps = eps # 小常数 1e-7
-        self.v = [] # 累计平方梯度
+        self.s = [] # 累计平方梯度
         for parameter in self.module.parameters():
-            self.v.append(zeros_like(parameter.data))
+            self.s.append(np.zeros_like(parameter.data))
 
     def step(self):
         for i,parameter in enumerate(self.module.parameters()):
-            v = self.alpha * self.v[i]
-            v = v + (1-self.alpha) * parameter.grad**2
-            self.v[i] = v
-            eta = self.lr / (np.sqrt(v.data) + self.eps)
-            parameter -=  parameter.grad * eta
+            s = self.beta * self.s[i] + (1-self.beta) * parameter.grad.data**2
+            self.s[i] = s
+            eta = self.lr * parameter.grad.data / (np.sqrt(s + self.eps))
+            parameter -=   eta
 
+class Adam(Optimizer):
 
-class Adam:
-    def __init__(self, module: Module, lr: float = 1e-3) -> None:
-        raise NotImplementedError("Adam优化器可咋整啊，伙计")
-        self.lr = lr
-        self.module = module
+    def __init__(self, module: Module, lr: float = 1e-3, betas:float=(0.9, 0.999),eps:float=1e-7) -> None:
+        super(Adam,self).__init__(lr, module)
+        self.betas = betas
+        self.eps = eps
+        self.v = []
+        self.s = []
+        for parameter in self.module.parameters():
+            self.v.append(np.zeros_like(parameter.data))
+            self.s.append(np.zeros_like(parameter.data))
+
 
     def step(self) -> None:
-        pass
+        for i,parameter in enumerate(self.module.parameters()):
+            v = self.betas[0] * self.v[i] + (1 - self.betas[0]) * parameter.grad.data
+            s = self.betas[1] * self.s[i] + (1 - self.betas[1]) * parameter.grad.data ** 2
+            self.v[i], self.s[i] = v, s
+            eta = self.lr * v/np.sqrt(s+self.eps)
+            parameter -= eta
